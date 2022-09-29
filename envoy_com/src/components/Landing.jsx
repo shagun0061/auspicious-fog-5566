a7f8fd89821c8e9607dbb144f19424044ddd4ef5
shagun <shagun08081999@gmail.com>
2022-09-29 23:27:29 +0530

    day-3

--- envoy_com/src/components/Landing.jsx
+++ envoy_com/src/components/Landing.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import {Image} from '@chakra-ui/react'
-import product from "../projext1.PNG"
+import img1 from "../projext1.PNG"
 function Landing() {
   return (
     <div>
-    <Image src='https://drive.google.com/file/d/1V0jOIP5bZQ03_VQ9QZ1e7jmwjVMTmtlm/view?usp=sharing'/>
+    <Image src={img1}/>
     </div>
   )
 }
